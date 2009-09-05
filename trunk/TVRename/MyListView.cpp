76805241c0fc413ffc28478c41c9c1baa4b782b9
tvrename@tvrename.com <tvrename@tvrename.com@e8ade50a-9955-11de-9e98-d1f6c90d23ca>
2009-09-05 00:23:12 +0000

    Added GPL license and other URLs at top of all source files.

--- trunk/TVRename/MyListView.cpp
+++ trunk/TVRename/MyListView.cpp
@@ -1,2 +1,9 @@
+//
+// Main website for TVRename is http://tvrename.com
+//
+// Source code available at http://code.google.com/p/tvrename/
+//
+// This code is released under GPLv3 http://www.gnu.org/licenses/gpl.html
+//
 #include "StdAfx.h"
 #include "MyListView.h"
--- trunk/TVRename/stdafx.cpp
+++ trunk/TVRename/stdafx.cpp
@@ -1,3 +1,10 @@
+//
+// Main website for TVRename is http://tvrename.com
+//
+// Source code available at http://code.google.com/p/tvrename/
+//
+// This code is released under GPLv3 http://www.gnu.org/licenses/gpl.html
+//
 // stdafx.cpp : source file that includes just the standard includes
 // TVRename.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
