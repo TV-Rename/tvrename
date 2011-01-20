0f0b9ff4998d15c463f02ca34493bf8cec96688d
tvrename@tvrename.com <tvrename@tvrename.com@e8ade50a-9955-11de-9e98-d1f6c90d23ca>
2011-01-20 09:02:19 +0000

    Copying trunk version to jg's branch

---
+++ branches/jg/TVRename#/Forms/FolderMonitorProgress.Designer.cs
@@ -0,0 +1,106 @@
+﻿//
+// Main website for TVRename is http://tvrename.com
+//
+// Source code available at http://code.google.com/p/tvrename/
+//
+// This code is released under GPLv3 http://www.gnu.org/licenses/gpl.html
+//
+
+
+namespace TVRename
+{
+    partial class FolderMonitorProgress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bnCancel = new System.Windows.Forms.Button();
+            this.lbMessage = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pbProgress = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // bnCancel
+            // 
+            this.bnCancel.Location = new System.Drawing.Point(114, 59);
+            this.bnCancel.Name = "bnCancel";
+            this.bnCancel.Size = new System.Drawing.Size(75, 23);
+            this.bnCancel.TabIndex = 0;
+            this.bnCancel.Text = "Cancel";
+            this.bnCancel.UseVisualStyleBackColor = true;
+            this.bnCancel.Click += new System.EventHandler(this.bnCancel_Click);
+            // 
+            // lbMessage
+            // 
+            this.lbMessage.AutoSize = true;
+            this.lbMessage.Location = new System.Drawing.Point(12, 9);
+            this.lbMessage.Name = "lbMessage";
+            this.lbMessage.Size = new System.Drawing.Size(56, 13);
+            this.lbMessage.TabIndex = 2;
+            this.lbMessage.Text = "Working...";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pbProgress
+            // 
+            this.pbProgress.Location = new System.Drawing.Point(12, 27);
+            this.pbProgress.Name = "pbProgress";
+            this.pbProgress.Size = new System.Drawing.Size(177, 23);
+            this.pbProgress.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pbProgress.TabIndex = 29;
+            // 
+            // FolderMonitorProgress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(201, 92);
+            this.Controls.Add(this.pbProgress);
+            this.Controls.Add(this.lbMessage);
+            this.Controls.Add(this.bnCancel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FolderMonitorProgress";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Folder Monitor Progress";
+            this.Load += new System.EventHandler(this.FolderMonitorProgress_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bnCancel;
+
+        private System.Windows.Forms.Label lbMessage;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar pbProgress;
+    }
+}
