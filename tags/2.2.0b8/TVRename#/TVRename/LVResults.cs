633d146b4163a7bc5aa83d44a934f473d03e4119
tvrename@tvrename.com <tvrename@tvrename.com@e8ade50a-9955-11de-9e98-d1f6c90d23ca>
2011-06-18 03:40:29 +0000

    Tag: TVRename 2.2.0b8

---
+++ tags/2.2.0b8/TVRename#/TVRename/LVResults.cs
@@ -0,0 +1,118 @@
+﻿// 
+// Main website for TVRename is http://tvrename.com
+// 
+// Source code available at http://code.google.com/p/tvrename/
+// 
+// This code is released under GPLv3 http://www.gnu.org/licenses/gpl.html
+// 
+namespace TVRename
+{
+    using System.Windows.Forms;
+
+    public class LVResults
+    {
+        #region WhichResults enum
+
+        public enum WhichResults
+        {
+            Checked,
+            Selected,
+            All
+        }
+
+        #endregion
+
+        public bool AllSameType;
+
+        public System.Collections.Generic.List<ActionCopyMoveRename> CopyMove;
+        public int Count;
+        public System.Collections.Generic.List<ActionDownload> Download;
+        public ScanListItemList FlatList;
+        public System.Collections.Generic.List<ItemMissing> Missing;
+        public System.Collections.Generic.List<ActionNFO> NFO;
+        public System.Collections.Generic.List<ActionRSS> RSS;
+        public System.Collections.Generic.List<ActionCopyMoveRename> Rename;
+        public System.Collections.Generic.List<ItemuTorrenting> uTorrenting;
+
+        public LVResults(ListView lv, bool @checked) // if not checked, then selected items
+        {
+            this.Go(lv, @checked ? WhichResults.Checked : WhichResults.Selected);
+        }
+
+        public LVResults(ListView lv, WhichResults which)
+        {
+            this.Go(lv, which);
+        }
+
+        public void Go(ListView lv, WhichResults which)
+        {
+            this.uTorrenting = new System.Collections.Generic.List<ItemuTorrenting>();
+            this.Missing = new System.Collections.Generic.List<ItemMissing>();
+            this.RSS = new System.Collections.Generic.List<ActionRSS>();
+            this.CopyMove = new System.Collections.Generic.List<ActionCopyMoveRename>();
+            this.Rename = new System.Collections.Generic.List<ActionCopyMoveRename>();
+            this.Download = new System.Collections.Generic.List<ActionDownload>();
+            this.NFO = new System.Collections.Generic.List<ActionNFO>();
+            this.FlatList = new ScanListItemList();
+
+            System.Collections.Generic.List<ListViewItem> sel = new System.Collections.Generic.List<ListViewItem>();
+            if (which == WhichResults.Checked)
+            {
+                ListView.CheckedListViewItemCollection ss = lv.CheckedItems;
+                foreach (ListViewItem lvi in ss)
+                    sel.Add(lvi);
+            }
+            else if (which == WhichResults.Selected)
+            {
+                ListView.SelectedListViewItemCollection ss = lv.SelectedItems;
+                foreach (ListViewItem lvi in ss)
+                    sel.Add(lvi);
+            }
+            else // all
+            {
+                foreach (ListViewItem lvi in lv.Items)
+                    sel.Add(lvi);
+            }
+
+            this.Count = sel.Count;
+
+            if (sel.Count == 0)
+                return;
+
+            System.Type firstType = ((Item) (sel[0].Tag)).GetType();
+
+            this.AllSameType = true;
+            foreach (ListViewItem lvi in sel)
+            {
+                if (lvi == null)
+                    continue;
+
+                Item action = (Item) (lvi.Tag);
+                if (action is ScanListItem)
+                    this.FlatList.Add(action as ScanListItem);
+
+                if (action.GetType() != firstType)
+                    this.AllSameType = false;
+
+                if (action is ActionCopyMoveRename)
+                {
+                    ActionCopyMoveRename cmr = action as ActionCopyMoveRename;
+                    if (cmr.Operation == ActionCopyMoveRename.Op.Rename)
+                        this.Rename.Add(cmr);
+                    else // copy/move
+                        this.CopyMove.Add(cmr);
+                }
+                else if (action is ActionDownload)
+                    this.Download.Add((ActionDownload) (action));
+                else if (action is ActionRSS)
+                    this.RSS.Add((ActionRSS) (action));
+                else if (action is ItemMissing)
+                    this.Missing.Add((ItemMissing) (action));
+                else if (action is ActionNFO)
+                    this.NFO.Add((ActionNFO) (action));
+                else if (action is ItemuTorrenting)
+                    this.uTorrenting.Add((ItemuTorrenting) (action));
+            }
+        }
+    }
+}
---
+++ tags/2.2.0b8/TVRename#/TVRename/PathManager.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace TVRename
+{
+    public class PathManager
+    {
+        public const string TVDBFileName = "TheTVDB.xml";
+        public const string SettingsFileName = "TVRenameSettings.xml";
+        const string LayoutFileName = "TVRenameLayout.dat";
+        const string UILayoutFileName = "Layout.xml";
+        const string StatisticsFileName = "Statistics.xml";
+
+        private static string userDefinedBasePath;
+
+        public static void SetUserDefinedBasePath(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentNullException("path");
+            }
+            if (System.IO.File.Exists(path))
+            {
+                throw new ArgumentException("path");
+            }
+            path = System.IO.Path.GetFullPath(path); // Get absolute path, in case the given path was a relative one. This will make the Path absolute depending on the Environment.CurrentDirectory.
+            // Why are we getting a absolute path here ? Simply because it is not guaranteed that the Environment.CurrentDirectory will not change a some point during runtime and then all bets are off were the Files are going to be saved, which would be fatal to the data integrity.(Saved changes might go to some file nobody even knew about )
+            if (!System.IO.Directory.Exists(path))
+            {
+                System.IO.Directory.CreateDirectory(path);
+            }
+            userDefinedBasePath = path;
+        }
+
+        protected static FileInfo GetFileInfo(string path, string file)
+        {
+            return new FileInfo(System.IO.Path.Combine(path, file));
+        }
+
+        public static FileInfo StatisticsFile
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(userDefinedBasePath))
+                {
+                    return GetFileInfo(userDefinedBasePath, StatisticsFileName);
+                }
+                else
+                {
+                    return GetFileInfo(System.Windows.Forms.Application.UserAppDataPath, StatisticsFileName);
+                }
+            }
+        }
+
+        public static FileInfo LayoutFile
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(userDefinedBasePath))
+                {
+                    return GetFileInfo(userDefinedBasePath, LayoutFileName);
+                }
+                else
+                {
+                    return GetFileInfo(System.Windows.Forms.Application.UserAppDataPath, LayoutFileName);
+                }
+            }
+        }
+
+        public static FileInfo UILayoutFile
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(userDefinedBasePath))
+                {
+                    return GetFileInfo(userDefinedBasePath, UILayoutFileName);
+                }
+                else
+                {
+                    return GetFileInfo(System.Windows.Forms.Application.UserAppDataPath, UILayoutFileName);
+                }
+            }
+        }
+
+        public static FileInfo TVDBFile
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(userDefinedBasePath))
+                {
+                    return GetFileInfo(userDefinedBasePath, TVDBFileName);
+                }
+                else
+                {
+                    return GetFileInfo(System.Windows.Forms.Application.UserAppDataPath, TVDBFileName);
+                }
+            }
+        }
+
+        public static FileInfo TVDocSettingsFile
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(userDefinedBasePath))
+                {
+                    return GetFileInfo(userDefinedBasePath, SettingsFileName);
+                }
+                else
+                {
+                    return GetFileInfo(System.Windows.Forms.Application.UserAppDataPath, SettingsFileName);
+                }
+            }
+        }
+
+    }
+}
