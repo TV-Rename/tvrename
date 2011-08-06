4fe8da47ac9ecbc85e152be53474b3059f204663
scphantm <scphantm@gmail.com>
2011-08-06 19:48:23 +0000

    first version of db storage system.  don't trust it, still tons missing and it won't compile

---
+++ TVRename#/db_access/document/Images.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TVRename.db_access.document
+{
+    class Images
+    {
+        public enum ImageType
+        {
+            banner,
+            fanart,
+            poster
+        }
+
+        public ImageType type { get; set; }
+        public string path { get; set; }
+    }
+}
