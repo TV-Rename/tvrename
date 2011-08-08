919bd367e93fe4b1a0d078eb391a58fcf73a8ce5
scphantm@gmail.com <scphantm@gmail.com@e8ade50a-9955-11de-9e98-d1f6c90d23ca>
2011-08-08 23:54:52 +0000

    organized the repository system

---
+++ branches/tvrename3/TVRename#/db_access/repository/ConfigRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TVRename.db_access.documents;
+
+namespace TVRename.db_access.repository
+{
+    class ConfigManager
+    {
+        public ConfigDocument loadConfigSettings()
+        {
+            return new ConfigDocument();
+        }
+    }
+}
