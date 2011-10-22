262596709755e69c305617c452585cbd993c74d5
tvrename@tvrename.com <tvrename@tvrename.com@e8ade50a-9955-11de-9e98-d1f6c90d23ca>
2011-10-22 08:06:09 +0000

    New branch for fs

---
+++ branches/fs/TVRename#/Forms/AddModifyRule.Designer.cs
@@ -0,0 +1,305 @@
+﻿//
+// Main website for TVRename is http://tvrename.com
+//
+// Source code available at http://code.google.com/p/tvrename/
+//
+// This code is released under GPLv3 http://www.gnu.org/licenses/gpl.html
+//
+
+
+namespace TVRename
+{
+    partial class AddModifyRule
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = (new System.ComponentModel.ComponentResourceManager(typeof(AddModifyRule)));
+            this.rbRemove = (new System.Windows.Forms.RadioButton());
+            this.rbSwap = (new System.Windows.Forms.RadioButton());
+            this.rbMerge = (new System.Windows.Forms.RadioButton());
+            this.rbInsert = (new System.Windows.Forms.RadioButton());
+            this.label1 = (new System.Windows.Forms.Label());
+            this.txtLabel1 = (new System.Windows.Forms.Label());
+            this.txtLabel2 = (new System.Windows.Forms.Label());
+            this.txtValue1 = (new System.Windows.Forms.TextBox());
+            this.txtValue2 = (new System.Windows.Forms.TextBox());
+            this.txtWithNameLabel = (new System.Windows.Forms.Label());
+            this.txtUserText = (new System.Windows.Forms.TextBox());
+            this.bnOK = (new System.Windows.Forms.Button());
+            this.bnCancel = (new System.Windows.Forms.Button());
+            this.rbIgnore = (new System.Windows.Forms.RadioButton());
+            this.rbRename = (new System.Windows.Forms.RadioButton());
+            this.txtLeaveBlank = (new System.Windows.Forms.Label());
+            this.rbSplit = (new System.Windows.Forms.RadioButton());
+            this.rbCollapse = (new System.Windows.Forms.RadioButton());
+            this.SuspendLayout();
+            // 
+            // rbRemove
+            // 
+            this.rbRemove.AutoSize = true;
+            this.rbRemove.Location = new System.Drawing.Point(61, 58);
+            this.rbRemove.Name = "rbRemove";
+            this.rbRemove.Size = new System.Drawing.Size(236, 17);
+            this.rbRemove.TabIndex = 2;
+            this.rbRemove.TabStop = true;
+            this.rbRemove.Text = "R&emove : Remove episode(s) from the series";
+            this.rbRemove.UseVisualStyleBackColor = true;
+            this.rbRemove.Click += new System.EventHandler(rb_Click);
+            // 
+            // rbSwap
+            // 
+            this.rbSwap.AutoSize = true;
+            this.rbSwap.Location = new System.Drawing.Point(61, 81);
+            this.rbSwap.Name = "rbSwap";
+            this.rbSwap.Size = new System.Drawing.Size(204, 17);
+            this.rbSwap.TabIndex = 3;
+            this.rbSwap.TabStop = true;
+            this.rbSwap.Text = "&Swap : Swap position of two episodes";
+            this.rbSwap.UseVisualStyleBackColor = true;
+            this.rbSwap.Click += new System.EventHandler(rb_Click);
+            // 
+            // rbMerge
+            // 
+            this.rbMerge.AutoSize = true;
+            this.rbMerge.Location = new System.Drawing.Point(61, 104);
+            this.rbMerge.Name = "rbMerge";
+            this.rbMerge.Size = new System.Drawing.Size(239, 17);
+            this.rbMerge.TabIndex = 4;
+            this.rbMerge.TabStop = true;
+            this.rbMerge.Text = "&Merge : Merge episodes into multi-episode file";
+            this.rbMerge.UseVisualStyleBackColor = true;
+            this.rbMerge.Click += new System.EventHandler(rb_Click);
+            // 
+            // rbInsert
+            // 
+            this.rbInsert.AutoSize = true;
+            this.rbInsert.Location = new System.Drawing.Point(61, 127);
+            this.rbInsert.Name = "rbInsert";
+            this.rbInsert.Size = new System.Drawing.Size(240, 17);
+            this.rbInsert.TabIndex = 5;
+            this.rbInsert.TabStop = true;
+            this.rbInsert.Text = "Inser&t : Insert another episode into the season";
+            this.rbInsert.UseVisualStyleBackColor = true;
+            this.rbInsert.Click += new System.EventHandler(rb_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Action:";
+            // 
+            // txtLabel1
+            // 
+            this.txtLabel1.AutoSize = true;
+            this.txtLabel1.Location = new System.Drawing.Point(13, 217);
+            this.txtLabel1.Name = "txtLabel1";
+            this.txtLabel1.Size = new System.Drawing.Size(42, 13);
+            this.txtLabel1.TabIndex = 6;
+            this.txtLabel1.Text = "Label1:";
+            // 
+            // txtLabel2
+            // 
+            this.txtLabel2.AutoSize = true;
+            this.txtLabel2.Location = new System.Drawing.Point(13, 242);
+            this.txtLabel2.Name = "txtLabel2";
+            this.txtLabel2.Size = new System.Drawing.Size(42, 13);
+            this.txtLabel2.TabIndex = 8;
+            this.txtLabel2.Text = "Label2:";
+            // 
+            // txtValue1
+            // 
+            this.txtValue1.Location = new System.Drawing.Point(80, 213);
+            this.txtValue1.Name = "txtValue1";
+            this.txtValue1.Size = new System.Drawing.Size(100, 20);
+            this.txtValue1.TabIndex = 7;
+            // 
+            // txtValue2
+            // 
+            this.txtValue2.Location = new System.Drawing.Point(80, 239);
+            this.txtValue2.Name = "txtValue2";
+            this.txtValue2.Size = new System.Drawing.Size(100, 20);
+            this.txtValue2.TabIndex = 9;
+            // 
+            // txtWithNameLabel
+            // 
+            this.txtWithNameLabel.AutoSize = true;
+            this.txtWithNameLabel.Location = new System.Drawing.Point(13, 269);
+            this.txtWithNameLabel.Name = "txtWithNameLabel";
+            this.txtWithNameLabel.Size = new System.Drawing.Size(63, 13);
+            this.txtWithNameLabel.TabIndex = 10;
+            this.txtWithNameLabel.Text = "New Name:";
+            // 
+            // txtUserText
+            // 
+            this.txtUserText.Location = new System.Drawing.Point(80, 266);
+            this.txtUserText.Name = "txtUserText";
+            this.txtUserText.Size = new System.Drawing.Size(254, 20);
+            this.txtUserText.TabIndex = 11;
+            // 
+            // bnOK
+            // 
+            this.bnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.bnOK.Location = new System.Drawing.Point(175, 308);
+            this.bnOK.Name = "bnOK";
+            this.bnOK.Size = new System.Drawing.Size(75, 23);
+            this.bnOK.TabIndex = 12;
+            this.bnOK.Text = "OK";
+            this.bnOK.UseVisualStyleBackColor = true;
+            this.bnOK.Click += bnOK_Click;
+            // 
+            // bnCancel
+            // 
+            this.bnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.bnCancel.Location = new System.Drawing.Point(256, 308);
+            this.bnCancel.Name = "bnCancel";
+            this.bnCancel.Size = new System.Drawing.Size(75, 23);
+            this.bnCancel.TabIndex = 13;
+            this.bnCancel.Text = "Cancel";
+            this.bnCancel.UseVisualStyleBackColor = true;
+            // 
+            // rbIgnore
+            // 
+            this.rbIgnore.AutoSize = true;
+            this.rbIgnore.Location = new System.Drawing.Point(61, 12);
+            this.rbIgnore.Name = "rbIgnore";
+            this.rbIgnore.Size = new System.Drawing.Size(246, 17);
+            this.rbIgnore.TabIndex = 1;
+            this.rbIgnore.TabStop = true;
+            this.rbIgnore.Text = "&Ignore : Don\'t rename or check for episode(s)";
+            this.rbIgnore.UseVisualStyleBackColor = true;
+            this.rbIgnore.Click += new System.EventHandler(rb_Click);
+            // 
+            // rbRename
+            // 
+            this.rbRename.AutoSize = true;
+            this.rbRename.Location = new System.Drawing.Point(61, 35);
+            this.rbRename.Name = "rbRename";
+            this.rbRename.Size = new System.Drawing.Size(203, 17);
+            this.rbRename.TabIndex = 1;
+            this.rbRename.TabStop = true;
+            this.rbRename.Text = "&Rename : Set episode name manually";
+            this.rbRename.UseVisualStyleBackColor = true;
+            this.rbRename.Click += new System.EventHandler(rb_Click);
+            // 
+            // txtLeaveBlank
+            // 
+            this.txtLeaveBlank.AutoSize = true;
+            this.txtLeaveBlank.Location = new System.Drawing.Point(77, 289);
+            this.txtLeaveBlank.Name = "txtLeaveBlank";
+            this.txtLeaveBlank.Size = new System.Drawing.Size(173, 13);
+            this.txtLeaveBlank.TabIndex = 10;
+            this.txtLeaveBlank.Text = "(Leave blank for automatic naming)";
+            // 
+            // rbSplit
+            // 
+            this.rbSplit.AutoSize = true;
+            this.rbSplit.Location = new System.Drawing.Point(61, 150);
+            this.rbSplit.Name = "rbSplit";
+            this.rbSplit.Size = new System.Drawing.Size(221, 17);
+            this.rbSplit.TabIndex = 5;
+            this.rbSplit.TabStop = true;
+            this.rbSplit.Text = "S&plit: Make one episode count as multiple";
+            this.rbSplit.UseVisualStyleBackColor = true;
+            this.rbSplit.Click += new System.EventHandler(rb_Click);
+            // 
+            // rbCollapse
+            // 
+            this.rbCollapse.AutoSize = true;
+            this.rbCollapse.Location = new System.Drawing.Point(61, 173);
+            this.rbCollapse.Name = "rbCollapse";
+            this.rbCollapse.Size = new System.Drawing.Size(217, 17);
+            this.rbCollapse.TabIndex = 5;
+            this.rbCollapse.TabStop = true;
+            this.rbCollapse.Text = "&Collapse: Merge episodes, and renumber";
+            this.rbCollapse.UseVisualStyleBackColor = true;
+            this.rbCollapse.Click += new System.EventHandler(rb_Click);
+            // 
+            // AddModifyRule
+            // 
+            this.AcceptButton = this.bnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6, 13);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.bnCancel;
+            this.ClientSize = new System.Drawing.Size(341, 341);
+            this.Controls.Add(this.bnCancel);
+            this.Controls.Add(this.bnOK);
+            this.Controls.Add(this.txtUserText);
+            this.Controls.Add(this.txtValue2);
+            this.Controls.Add(this.txtValue1);
+            this.Controls.Add(this.txtLeaveBlank);
+            this.Controls.Add(this.txtWithNameLabel);
+            this.Controls.Add(this.txtLabel2);
+            this.Controls.Add(this.txtLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rbRename);
+            this.Controls.Add(this.rbIgnore);
+            this.Controls.Add(this.rbCollapse);
+            this.Controls.Add(this.rbSplit);
+            this.Controls.Add(this.rbInsert);
+            this.Controls.Add(this.rbMerge);
+            this.Controls.Add(this.rbSwap);
+            this.Controls.Add(this.rbRemove);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AddModifyRule";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Add/Modify Rule";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbRemove;
+        private System.Windows.Forms.RadioButton rbSwap;
+
+        private System.Windows.Forms.RadioButton rbMerge;
+        private System.Windows.Forms.RadioButton rbInsert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtLabel1;
+        private System.Windows.Forms.Label txtLabel2;
+        private System.Windows.Forms.TextBox txtValue1;
+        private System.Windows.Forms.TextBox txtValue2;
+        private System.Windows.Forms.Label txtWithNameLabel;
+
+        private System.Windows.Forms.TextBox txtUserText;
+
+        private System.Windows.Forms.Button bnOK;
+        private System.Windows.Forms.Button bnCancel;
+        private System.Windows.Forms.RadioButton rbIgnore;
+        private System.Windows.Forms.RadioButton rbRename;
+        private System.Windows.Forms.Label txtLeaveBlank;
+        private System.Windows.Forms.RadioButton rbSplit;
+        private System.Windows.Forms.RadioButton rbCollapse;
+    }
+}
---
+++ branches/fs/TVRename#/ItemsAndActions/ActionNFO.cs
@@ -0,0 +1,371 @@
+﻿// 
+// Main website for TVRename is http://tvrename.com
+// 
+// Source code available at http://code.google.com/p/tvrename/
+// 
+// This code is released under GPLv3 http://www.gnu.org/licenses/gpl.html
+// 
+namespace TVRename
+{
+    using System;
+    using System.IO;
+    using System.Windows.Forms;
+    using System.Xml;
+
+    public class ActionNFO : Item, Action, ScanListItem
+    {
+        public ShowItem SI; // if for an entire show, rather than specific episode
+        public FileInfo Where;
+
+        public ActionNFO(FileInfo nfo, ProcessedEpisode pe)
+        {
+            this.SI = null;
+            this.Episode = pe;
+            this.Where = nfo;
+        }
+
+        public ActionNFO(FileInfo nfo, ShowItem si)
+        {
+            this.SI = si;
+            this.Episode = null;
+            this.Where = nfo;
+        }
+
+        #region Action Members
+
+        public string Name
+        {
+            get { return "Write NFO"; }
+        }
+
+        public bool Done { get; private set; }
+        public bool Error { get; private set; }
+        public string ErrorText { get; set; }
+
+        public string ProgressText
+        {
+            get { return this.Where.Name; }
+        }
+
+        public double PercentDone
+        {
+            get { return this.Done ? 100 : 0; }
+        }
+
+        public long SizeOfWork
+        {
+            get { return 10000; }
+        }
+
+        public bool Go(TVSettings tvsettings, ref bool pause)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings
+            {
+                Indent = true,
+                NewLineOnAttributes = true
+            };
+            // "try" and silently fail.  eg. when file is use by other...
+            XmlWriter writer;
+            try
+            {
+                //                XmlWriter writer = XmlWriter.Create(this.Where.FullName, settings);
+                writer = XmlWriter.Create(this.Where.FullName, settings);
+                if (writer == null)
+                    return false;
+            }
+            catch (Exception)
+            {
+                this.Done = true;
+                return true;
+            }
+
+            if (this.Episode != null) // specific episode
+            {
+                // See: http://xbmc.org/wiki/?title=Import_-_Export_Library#TV_Episodes
+                writer.WriteStartElement("episodedetails");
+                writer.WriteStartElement("title");
+                writer.WriteValue(this.Episode.Name);
+                writer.WriteEndElement();
+                writer.WriteStartElement("rating");
+                writer.WriteValue(this.Episode.EpisodeRating);
+                writer.WriteEndElement();
+                writer.WriteStartElement("season");
+                writer.WriteValue(this.Episode.SeasonNumber);
+                writer.WriteEndElement();
+                writer.WriteStartElement("episode");
+                writer.WriteValue(this.Episode.EpNum);
+                writer.WriteEndElement();
+                writer.WriteStartElement("plot");
+                writer.WriteValue(this.Episode.Overview);
+                writer.WriteEndElement();
+                writer.WriteStartElement("aired");
+                if (this.Episode.FirstAired != null)
+                    writer.WriteValue(this.Episode.FirstAired.Value.ToString("yyyy-MM-dd"));
+                writer.WriteEndElement();
+
+                if (this.Episode.SI != null)
+                {
+                    WriteInfo(writer, this.Episode.SI, "ContentRating", "mpaa");
+                }
+
+                //Director(s)
+                if (!String.IsNullOrEmpty(this.Episode.EpisodeDirector))
+                {
+                    string EpDirector = this.Episode.EpisodeDirector;
+                    if (!string.IsNullOrEmpty(EpDirector))
+                    {
+                        foreach (string Daa in EpDirector.Split('|'))
+                        {
+                            if (string.IsNullOrEmpty(Daa))
+                                continue;
+
+                            writer.WriteStartElement("director");
+                            writer.WriteValue(Daa);
+                            writer.WriteEndElement();
+                        }
+                    }
+                }
+
+                //Writers(s)
+                if (!String.IsNullOrEmpty(this.Episode.Writer))
+                {
+                    string EpWriter = this.Episode.Writer;
+                    if (!string.IsNullOrEmpty(EpWriter))
+                    {
+                        writer.WriteStartElement("credits");
+                        writer.WriteValue(EpWriter);
+                        writer.WriteEndElement();
+                    }
+                }
+
+                // Guest Stars...
+                if (!String.IsNullOrEmpty(this.Episode.EpisodeGuestStars))
+                {
+                    string RecurringActors = "";
+
+                    if (this.Episode.SI != null)
+                    {
+                        RecurringActors = this.Episode.SI.TheSeries().GetItem("Actors");
+                    }
+
+                    string GuestActors = this.Episode.EpisodeGuestStars;
+                    if (!string.IsNullOrEmpty(GuestActors))
+                    {
+                        foreach (string Gaa in GuestActors.Split('|'))
+                        {
+                            if (string.IsNullOrEmpty(Gaa))
+                                continue;
+
+                            // Skip if the guest actor is also in the overal recurring list
+                            if (!string.IsNullOrEmpty(RecurringActors) && RecurringActors.Contains(Gaa))
+                            {
+                                continue;
+                            }
+
+                            writer.WriteStartElement("actor");
+                            writer.WriteStartElement("name");
+                            writer.WriteValue(Gaa);
+                            writer.WriteEndElement(); // name
+                            writer.WriteEndElement(); // actor
+                        }
+                    }
+                }
+
+                // actors...
+                if (this.Episode.SI != null)
+                {
+                    string actors = this.Episode.SI.TheSeries().GetItem("Actors");
+                    if (!string.IsNullOrEmpty(actors))
+                    {
+                        foreach (string aa in actors.Split('|'))
+                        {
+                            if (string.IsNullOrEmpty(aa))
+                                continue;
+
+                            writer.WriteStartElement("actor");
+                            writer.WriteStartElement("name");
+                            writer.WriteValue(aa);
+                            writer.WriteEndElement(); // name
+                            writer.WriteEndElement(); // actor
+                        }
+                    }
+                }
+
+                writer.WriteEndElement(); // episodedetails
+            }
+            else if (this.SI != null) // show overview (tvshow.nfo)
+            {
+                // http://www.xbmc.org/wiki/?title=Import_-_Export_Library#TV_Shows
+
+                writer.WriteStartElement("tvshow");
+
+                writer.WriteStartElement("title");
+                writer.WriteValue(this.SI.ShowName);
+                writer.WriteEndElement();
+
+                writer.WriteStartElement("episodeguideurl");
+                writer.WriteValue(TheTVDB.BuildURL(true, true, this.SI.TVDBCode, this.SI.TVDB.PreferredLanguage(this.SI.TVDBCode)));
+                writer.WriteEndElement();
+
+                WriteInfo(writer, this.SI, "Overview", "plot");
+
+                string genre = this.SI.TheSeries().GetItem("Genre");
+                if (!string.IsNullOrEmpty(genre))
+                {
+                    genre = genre.Trim('|');
+                    genre = genre.Replace("|", " / ");
+                    writer.WriteStartElement("genre");
+                    writer.WriteValue(genre);
+                    writer.WriteEndElement();
+                }
+
+                WriteInfo(writer, this.SI, "FirstAired", "premiered");
+                WriteInfo(writer, this.SI, "Year", "year");
+                WriteInfo(writer, this.SI, "Rating", "rating");
+                WriteInfo(writer, this.SI, "Status", "status");
+
+                // actors...
+                string actors = this.SI.TheSeries().GetItem("Actors");
+                if (!string.IsNullOrEmpty(actors))
+                {
+                    foreach (string aa in actors.Split('|'))
+                    {
+                        if (string.IsNullOrEmpty(aa))
+                            continue;
+
+                        writer.WriteStartElement("actor");
+                        writer.WriteStartElement("name");
+                        writer.WriteValue(aa);
+                        writer.WriteEndElement(); // name
+                        writer.WriteEndElement(); // actor
+                    }
+                }
+
+                WriteInfo(writer, this.SI, "ContentRating", "mpaa");
+                WriteInfo(writer, this.SI, "IMDB_ID", "id");
+
+                writer.WriteStartElement("tvdbid");
+                writer.WriteValue(this.SI.TheSeries().TVDBCode);
+                writer.WriteEndElement();
+
+                string rt = this.SI.TheSeries().GetItem("Runtime");
+                if (!string.IsNullOrEmpty(rt))
+                {
+                    writer.WriteStartElement("runtime");
+                    writer.WriteValue(rt + " minutes");
+                    writer.WriteEndElement();
+                }
+
+                writer.WriteEndElement(); // tvshow
+            }
+
+            writer.Close();
+            this.Done = true;
+            return true;
+        }
+
+        #endregion
+
+        #region Item Members
+
+        public bool SameAs(Item o)
+        {
+            return (o is ActionNFO) && ((o as ActionNFO).Where == this.Where);
+        }
+
+        public int Compare(Item o)
+        {
+            ActionNFO nfo = o as ActionNFO;
+
+            if (this.Episode == null)
+                return 1;
+            if (nfo.Episode == null)
+                return -1;
+            return (this.Where.FullName + this.Episode.Name).CompareTo(nfo.Where.FullName + nfo.Episode.Name);
+        }
+
+        #endregion
+
+        #region ScanListItem Members
+
+        public IgnoreItem Ignore
+        {
+            get
+            {
+                if (this.Where == null)
+                    return null;
+                return new IgnoreItem(this.Where.FullName);
+            }
+        }
+
+        public ListViewItem ScanListViewItem
+        {
+            get
+            {
+                ListViewItem lvi = new ListViewItem();
+
+                if (this.Episode != null)
+                {
+                    lvi.Text = this.Episode.SI.ShowName;
+                    lvi.SubItems.Add(this.Episode.SeasonNumber.ToString());
+                    lvi.SubItems.Add(this.Episode.NumsAsString());
+                    DateTime? dt = this.Episode.GetAirDateDT(true);
+                    if ((dt != null) && (dt.Value.CompareTo(DateTime.MaxValue)) != 0)
+                        lvi.SubItems.Add(dt.Value.ToShortDateString());
+                    else
+                        lvi.SubItems.Add("");
+                }
+                else
+                {
+                    lvi.Text = this.SI.ShowName;
+                    lvi.SubItems.Add("");
+                    lvi.SubItems.Add("");
+                    lvi.SubItems.Add("");
+                }
+
+                lvi.SubItems.Add(this.Where.DirectoryName);
+                lvi.SubItems.Add(this.Where.Name);
+
+                lvi.Tag = this;
+
+                //lv->Items->Add(lvi);
+                return lvi;
+            }
+        }
+
+        string ScanListItem.TargetFolder
+        {
+            get
+            {
+                if (this.Where == null)
+                    return null;
+                return this.Where.DirectoryName;
+            }
+        }
+
+        public int ScanListViewGroup
+        {
+            get { return 6; }
+        }
+
+        public int IconNumber
+        {
+            get { return 7; }
+        }
+
+        public ProcessedEpisode Episode { get; private set; }
+
+        #endregion
+
+        private static void WriteInfo(XmlWriter writer, ShowItem si, string whichItem, string @as)
+        {
+            string t = si.TheSeries().GetItem(whichItem);
+            if (!string.IsNullOrEmpty(t))
+            {
+                writer.WriteStartElement(@as);
+                writer.WriteValue(t);
+                writer.WriteEndElement();
+            }
+        }
+    }
+}
