7cbcc2e79f6cf2deddddb91b8ff34ea4dc0a7af3
tvrename@tvrename.com <tvrename@tvrename.com@e8ade50a-9955-11de-9e98-d1f6c90d23ca>
2012-04-20 05:43:16 +0000

    New branch for mh

---
+++ branches/mh/TVRename#/App/AutoFolderMonitor.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace TVRename
+{
+    public class AutoFolderMonitor
+    {
+        private TVDoc mDoc;
+        private UI mUI;
+        private List<System.IO.FileSystemWatcher> Watchers = new List<System.IO.FileSystemWatcher>();
+        private System.Timers.Timer mScanDelayTimer;
+
+        public AutoFolderMonitor(TVDoc Doc,UI ui)
+        {
+            mDoc = Doc;
+            mUI = ui;
+
+            mScanDelayTimer = new System.Timers.Timer(1000);
+            mScanDelayTimer.Elapsed += new System.Timers.ElapsedEventHandler(mScanDelayTimer_Elapsed);
+            mScanDelayTimer.Stop();
+        }
+
+        public void SettingsChanged(bool monitor)
+        {
+            if (monitor)
+            {
+                StopMonitor();
+                StartMonitor();
+            }
+            else
+                StopMonitor();
+        }
+
+        public void StartMonitor()
+        {
+            foreach (string efi in this.mDoc.SearchFolders)
+            {
+                if (!File.Exists(efi) ||  // doesn't exist
+                    ((File.GetAttributes(efi) & FileAttributes.Directory) != (FileAttributes.Directory)) ) // not a folder
+                    continue;
+
+                FileSystemWatcher watcher = new FileSystemWatcher(efi);
+                watcher.Changed += new FileSystemEventHandler(watcher_Changed);
+                watcher.Created += new FileSystemEventHandler(watcher_Changed);
+                watcher.Renamed += new RenamedEventHandler(watcher_Changed);
+                //watcher.Deleted += new FileSystemEventHandler(watcher_Changed);
+                //watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.CreationTime;
+                watcher.IncludeSubdirectories = true;
+                watcher.EnableRaisingEvents = true;
+                Watchers.Add(watcher);
+            }
+        }
+
+        void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            mScanDelayTimer.Stop();
+            mScanDelayTimer.Start();
+        }
+
+        void mScanDelayTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            mScanDelayTimer.Stop();
+            this.StopMonitor();
+            if (mUI != null)
+            {
+                mUI.Invoke(mUI.AFMScan);
+                mUI.Invoke(mUI.AFMDoAll);
+            }
+            this.StartMonitor();
+        }
+
+        public void StopMonitor()
+        {
+            foreach (FileSystemWatcher watcher in Watchers)
+            {
+                watcher.EnableRaisingEvents = false;
+                watcher.Dispose();
+            }
+            Watchers.Clear();
+        }
+    }
+}
---
+++ branches/mh/TVRename#/Forms/EditRules.cs
@@ -0,0 +1,182 @@
+// 
+// Main website for TVRename is http://tvrename.com
+// 
+// Source code available at http://code.google.com/p/tvrename/
+// 
+// This code is released under GPLv3 http://www.gnu.org/licenses/gpl.html
+// 
+using System.Windows.Forms;
+
+namespace TVRename
+{
+    /// <summary>
+    /// Summary for EditRules
+    ///
+    /// WARNING: If you change the name of this class, you will need to change the
+    ///          'Resource File Name' property for the managed resource compiler tool
+    ///          associated with all .resx files this class depends on.  Otherwise,
+    ///          the designers will not be able to interact properly with localized
+    ///          resources associated with this form.
+    /// </summary>
+    public partial class EditRules : System.Windows.Forms.Form
+    {
+        private CustomName NameStyle;
+        private System.Collections.Generic.List<ShowRule> WorkingRuleSet;
+        private ProcessedEpisodeList mOriginalEps;
+        private ShowItem mSI;
+        private int mSeasonNumber;
+
+        public EditRules(ShowItem si, ProcessedEpisodeList originalEpList, int seasonNumber, CustomName style)
+        {
+            this.NameStyle = style;
+            this.InitializeComponent();
+
+            this.mSI = si;
+            this.mOriginalEps = originalEpList;
+            this.mSeasonNumber = seasonNumber;
+
+            if (si.SeasonRules.ContainsKey(seasonNumber))
+                this.WorkingRuleSet = new System.Collections.Generic.List<ShowRule>(si.SeasonRules[seasonNumber]);
+            else
+                this.WorkingRuleSet = new System.Collections.Generic.List<ShowRule>();
+
+            this.txtShowName.Text = si.ShowName;
+            this.txtSeasonNumber.Text = seasonNumber.ToString();
+
+            this.FillRuleList(false, 0);
+        }
+
+        private void bnAddRule_Click(object sender, System.EventArgs e)
+        {
+            ShowRule sr = new ShowRule();
+            AddModifyRule ar = new AddModifyRule(sr);
+
+            bool res = ar.ShowDialog() == System.Windows.Forms.DialogResult.OK;
+            if (res)
+                this.WorkingRuleSet.Add(sr);
+
+            this.FillRuleList(false, 0);
+        }
+
+        private void FillRuleList(bool keepSel, int adj)
+        {
+            System.Collections.Generic.List<int> sel = new System.Collections.Generic.List<int>();
+            if (keepSel)
+            {
+                foreach (int i in this.lvRuleList.SelectedIndices)
+                    sel.Add(i);
+            }
+
+            this.lvRuleList.Items.Clear();
+            foreach (ShowRule sr in this.WorkingRuleSet)
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = sr.ActionInWords();
+                lvi.SubItems.Add(sr.First == -1 ? "" : sr.First.ToString());
+                lvi.SubItems.Add(sr.Second == -1 ? "" : sr.Second.ToString());
+                lvi.SubItems.Add(sr.UserSuppliedText);
+                lvi.Tag = sr;
+                this.lvRuleList.Items.Add(lvi);
+            }
+
+            if (keepSel)
+            {
+                foreach (int i in sel)
+                {
+                    if (i < this.lvRuleList.Items.Count)
+                    {
+                        int n = i + adj;
+                        if ((n >= 0) && (n < this.lvRuleList.Items.Count))
+                            this.lvRuleList.Items[n].Selected = true;
+                    }
+                }
+            }
+
+            this.FillPreview();
+        }
+
+        private void bnEdit_Click(object sender, System.EventArgs e)
+        {
+            if (this.lvRuleList.SelectedItems.Count == 0)
+                return;
+            ShowRule sr = (ShowRule) (this.lvRuleList.SelectedItems[0].Tag);
+            AddModifyRule ar = new AddModifyRule(sr);
+            ar.ShowDialog(); // modifies rule in-place if OK'd
+            this.FillRuleList(false, 0);
+        }
+
+        private void bnDelRule_Click(object sender, System.EventArgs e)
+        {
+            if (this.lvRuleList.SelectedItems.Count == 0)
+                return;
+            ShowRule sr = (ShowRule) (this.lvRuleList.SelectedItems[0].Tag);
+
+            this.WorkingRuleSet.Remove(sr);
+            this.FillRuleList(false, 0);
+        }
+
+        private void bnRuleUp_Click(object sender, System.EventArgs e)
+        {
+            if (this.lvRuleList.SelectedIndices.Count != 1)
+                return;
+            int p = this.lvRuleList.SelectedIndices[0];
+            if (p <= 0)
+                return;
+
+            ShowRule sr = this.WorkingRuleSet[p];
+            this.WorkingRuleSet.RemoveAt(p);
+            this.WorkingRuleSet.Insert(p - 1, sr);
+
+            this.FillRuleList(true, -1);
+        }
+
+        private void bnRuleDown_Click(object sender, System.EventArgs e)
+        {
+            if (this.lvRuleList.SelectedIndices.Count != 1)
+                return;
+            int p = this.lvRuleList.SelectedIndices[0];
+            if (p >= (this.lvRuleList.Items.Count - 1))
+                return;
+
+            ShowRule sr = this.WorkingRuleSet[p];
+            this.WorkingRuleSet.RemoveAt(p);
+            this.WorkingRuleSet.Insert(p + 1, sr);
+            this.FillRuleList(true, +1);
+        }
+
+        private void lvRuleList_DoubleClick(object sender, System.EventArgs e)
+        {
+            this.bnEdit_Click(null, null);
+        }
+
+        private void bnOK_Click(object sender, System.EventArgs e)
+        {
+            this.mSI.SeasonRules[this.mSeasonNumber] = this.WorkingRuleSet;
+            this.Close();
+        }
+
+        private void bnCancel_Click(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FillPreview()
+        {
+            ProcessedEpisodeList pel = new ProcessedEpisodeList();
+
+            if (this.mOriginalEps != null)
+            {
+                foreach (ProcessedEpisode pe in this.mOriginalEps)
+                    pel.Add(new ProcessedEpisode(pe));
+
+                TVDoc.ApplyRules(pel, this.WorkingRuleSet, this.mSI);
+            }
+
+            this.lbEpsPreview.BeginUpdate();
+            this.lbEpsPreview.Items.Clear();
+            foreach (ProcessedEpisode pe in pel)
+                this.lbEpsPreview.Items.Add(this.NameStyle.NameForExt(pe, null, 0));
+            this.lbEpsPreview.EndUpdate();
+        }
+    }
+}
