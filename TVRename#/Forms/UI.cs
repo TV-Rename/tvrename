1575ba76d96fa6bd313d9de7f4dfa493f6849b73
Joe Biellik <joebiellik@gmail.com>
2018-01-03 14:14:59 +0000

    Rename settings to preferences

--- TVRename#/Forms/UI.cs
+++ TVRename#/Forms/UI.cs
@@ -811,7 +811,7 @@ namespace TVRename
             }
             else
             {
-                body += "<h2>Images are not being downloaded for this series. Please see Options -> Settings -> Media Center to reconfigure.</h2>";
+                body += "<h2>Images are not being downloaded for this series. Please see Options -> Preferences -> Media Center to reconfigure.</h2>";
             }
 
             return body;
