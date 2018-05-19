558a3f8a5c11ba76fc7fef6bd9b09c01720a1260
Mark Summerville <mark.summerville@gmail.com>
2018-05-19 12:40:32 +0800

    HTML updates for #405

--- TVRename#/Exporter/ShowsHTML.cs
+++ TVRename#/Exporter/ShowsHTML.cs
@@ -52,32 +52,29 @@ namespace TVRename
             bool ratingIsNumber = float.TryParse(si.TheSeries().GetSiteRating(), out float rating);
             string siteRating = ratingIsNumber && rating>0? rating+"/10":"";
 
-            return $@"     <div class=""card card-body"">
-        <div class=""row"">
+            return $@"<div class=""card card-body"">
+            <div class=""row"">
             <div class=""col-md-4"">
                 <img class=""show-poster rounded w-100"" src=""{posterURL}"" alt=""{si.ShowName} Show Poster""></div>
             <div class=""col-md-8"">
                 <div class=""row"">
-    <div class=""col-md-8""><h1>{si.ShowName}</h1></div>
-    <div class=""col-md-4 text-right""><h6>{yearRange} ({si.TheSeries().getStatus()})</h6><small class=""text-muted"">{episodeSummary } Episodes</small></div>
-    </div>
-    <div><blockquote>{si.TheSeries().GetOverview()}</blockquote></div>
-    <div><blockquote>{string.Join(", ",si.TheSeries().GetActors())}</blockquote></div>
-                
-                    <div class=""row"">
-    <div class=""col-md-4"">{stars}<br>{siteRating}</div>
-    <div class=""col-md-4 text-center"">{si.TheSeries().GetContentRating()}<br>{si.TheSeries().getNetwork()}</div>
-    <div class=""col-md-4 text-right"">{genreIcons}<br>{string.Join(", ",si.TheSeries().GetGenres()) }</div>
-</div>
-</div></div></div>";
+                    <div class=""col-md-8""><h1>{si.ShowName}</h1></div>
+                    <div class=""col-md-4 text-right""><h6>{yearRange} ({si.TheSeries().getStatus()})</h6><small class=""text-muted"">{episodeSummary } Episodes</small></div>
+                </div>
+            <div><blockquote>{si.TheSeries().GetOverview()}</blockquote></div>
+            <div><blockquote>{string.Join(", ",si.TheSeries().GetActors())}</blockquote></div>
+            <div class=""row"">
+                <div class=""col-md-4"">{stars}<br>{siteRating}</div>
+                <div class=""col-md-4 text-center"">{si.TheSeries().GetContentRating()}<br>{si.TheSeries().getNetwork()}</div>
+                <div class=""col-md-4 text-right"">{genreIcons}<br>{string.Join(", ",si.TheSeries().GetGenres()) }</div>
+            </div>
+            </div></div></div>";
         }
 
         private static string GenreIconHTML(string genre)
         {
             string[] availbleIcons = {"Action","Adventure","Animation","Children","Comedy","Crime","Documentary","Drama","Family","Fantasy","Food","Horror","Mini-Series","Mystery","Reality","Romance","Science-Fiction","Soap","Talk Show","Thriller","Travel","War","Western"};
             const string root = "https://www.tvrename.com/assets/images/GenreIcons/";
-
-
 
             return availbleIcons.Contains(genre)
                 ? $@"<img width=""30"" height=""30"" src=""{root}{genre}.svg"" alt=""{genre}"">"
@@ -108,9 +105,8 @@ namespace TVRename
         private static string HTMLHeader()
         {
             return @"<!DOCTYPE html>
-<html >
-<head>
-  <meta charset=""utf-8"">
+                <html><head>
+                <meta charset=""utf-8"">
                 <meta name = ""viewport"" content = ""width=device-width, initial-scale=1.0"" >
                 <title> TV Rename - Show Summary</title>
                 <link rel = ""stylesheet"" href = ""http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"" />
@@ -123,15 +119,13 @@ namespace TVRename
         private static string HTMLFooter()
         {
             return @"
-</div>
-  <script src=""//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js""></script>
-                   < script src = ""//cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"" ></ script >
-                < script src = ""//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"" ></ script >
-                < script >
-                $(document).ready(function(){ })
-            </ script >
-                </ body >
-                </ html >
+                </div>
+                <script src=""http://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js""></script>
+                <script src = ""http://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"" ></script>
+                <script src = ""http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"" ></script>
+                <script >$(document).ready(function(){ })</script>
+                </body>
+                </html>
                 ";
         }
     }
