49d851a19a6f09c431625d04102541cb998d2e77
Mark <mark.summerville@gmail.com>
2019-01-25 00:07:06 +0800

    Simplify and refactor SABFinder

--- TVRename/Utility/SAB/Result.cs
+++ TVRename/Utility/SAB/Result.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Text;
 using System.Xml.Serialization;
 // ReSharper disable All
 
@@ -11,7 +12,7 @@ namespace TVRename.SAB
     {
         public static Result Deserialize(byte[] data)
         {
-            MemoryStream ms = new MemoryStream(data);
+            MemoryStream ms = new MemoryStream(Encoding.ASCII.GetBytes("<?xml version=\"1.0\" encoding=\"UTF-8\" ?> <queue><noofslots_total>0</noofslots_total> <diskspace2_norm>2.2 T</diskspace2_norm> <paused>False</paused> <finish>0</finish> <speedlimit_abs></speedlimit_abs> <slots></slots> <speed>0  </speed> <size>0 B</size> <rating_enable>False</rating_enable> <eta>unknown</eta> <refresh_rate>1</refresh_rate> <start>0</start> <version>2.3.7</version> <diskspacetotal2>2794.39</diskspacetotal2> <limit>8888</limit> <diskspacetotal1>2794.39</diskspacetotal1> <status>Idle</status> <have_warnings>0</have_warnings> <cache_art>0</cache_art> <sizeleft>0 B</sizeleft> <finishaction>None</finishaction> <paused_all>False</paused_all> <quota>0 </quota> <have_quota>False</have_quota> <mbleft>0.00</mbleft> <diskspace2>2236.57</diskspace2> <diskspace1>2236.57</diskspace1> <scripts></scripts> <categories><category>*</category> </categories> <timeleft>0:00:00</timeleft> <pause_int>0</pause_int> <noofslots>0</noofslots> <mb>0.00</mb> <loadavg></loadavg> <cache_max>536870912</cache_max> <kbpersec>0.00</kbpersec> <speedlimit>100</speedlimit> <cache_size>0 B</cache_size> <left_quota>0 </left_quota> <diskspace1_norm>2.2 T</diskspace1_norm> <queue_details>0</queue_details> </queue>"));
             XmlSerializer serializer = new XmlSerializer(typeof(Result));
             try
             {
