6b86fe5ea73deb2f66f6f59c62f5aaf2fd62abbf
Mark <mark.summerville@gmail.com>
2020-01-13 08:04:01 +0800

    Try to track down NRE

--- TVRename/Finders/qBitTorrentFinder.cs
+++ TVRename/Finders/qBitTorrentFinder.cs
@@ -72,34 +72,43 @@ namespace TVRename
                 JToken settings = JToken.Parse(settingsString);
                 JArray currentDownloads = JArray.Parse(downloadsString);
 
+                if(currentDownloads is null || settings is null)
+                {
+                    LOGGER.Warn($"Could not get currentDownloads or settings from qBitTorrent: {settingsString} {currentDownloads}");
+                    return ret;
+                }
+
+                string savePath = (string)settings["save_path"]??string.Empty ;
+
                 foreach (JToken torrent in currentDownloads.Children())
                 {
-                    torrentDetailsString = JsonHelper.Obtain(GetApiUrl(qBitTorrentAPIPath.torrentDetails) + torrent["hash"]);
-                    JArray stuff2 = JArray.Parse(torrentDetailsString);
+                    (string hashCode,string torrentName) = ExtractTorrentDetails(torrent);
 
-                    foreach (JToken file in stuff2.Children())
+                    string url = GetApiUrl(qBitTorrentAPIPath.torrentDetails) + hashCode;
+                    torrentDetailsString = JsonHelper.Obtain(url);
+                    JArray torrentDetails = JArray.Parse(torrentDetailsString);
+
+                    if (torrentDetails is null)
                     {
-                        string downloadedFilename = file["name"].ToString();
-                        bool isOnHold = file["priority"].Value<int>() == 0;
+                        LOGGER.Warn($"Could not get details of downloads from {url} from qBitTorrent: {torrentDetailsString}");
+                        continue;
+                    }
+
+                    foreach (JToken file in torrentDetails.Children())
+                    {
+                        (string downloadedFilename, bool isOnHold, int percentComplete) = ExtractTorrentFileDetails(file);
+
                         if (!downloadedFilename.Contains(".!qB\\.unwanted\\") && !isOnHold)
                         {
-                            ret.Add(new TorrentEntry(torrent["name"].ToString(),
-                                settings["save_path"] + downloadedFilename,
-                                (int)(100 * file["progress"].ToObject<float>())));
+                            ret.Add(new TorrentEntry(torrentName,savePath + downloadedFilename,percentComplete));
                         }
                     }
 
-                    if (!stuff2.Children().Any())
+                    if (!torrentDetails.Children().Any())
                     {
-                        ret.Add(
-                            new TorrentEntry(
-                                torrent["name"].ToString()
-                                , TVSettings.Instance.FilenameFriendly(
-                                      settings["save_path"] + torrent["name"].ToString()) +
-                                  TVSettings.Instance.VideoExtensionsArray[0]
-                                , 0
-                            )
-                        );
+                        string proposedFilename = TVSettings.Instance.FilenameFriendly(savePath + torrentName) + TVSettings.Instance.VideoExtensionsArray[0];
+
+                        ret.Add(new TorrentEntry(torrentName, proposedFilename, 0));
                     }
                 }
             }
@@ -115,6 +124,23 @@ namespace TVRename
             }
 
             return ret;
+        }
+
+        private static (string downloadedFilename, bool isOnHold, int percentComplete) ExtractTorrentFileDetails(JToken file)
+        {
+            string downloadedFilename = file["name"].ToString();
+            bool isOnHold = file["priority"].Value<int>() == 0;
+            int percentComplete = (int)(100 * file["progress"].ToObject<float>());
+
+            return (downloadedFilename, isOnHold, percentComplete);
+        }
+
+        private static (string hashCode, string torrentName) ExtractTorrentDetails(JToken torrent)
+        {
+            string hashCode = (string)torrent["hash"];
+            string torrentName = (string)torrent["name"];
+
+            return (hashCode,torrentName);
         }
 
         [NotNull]
