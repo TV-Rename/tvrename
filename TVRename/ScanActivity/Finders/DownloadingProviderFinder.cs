e66bf5a898b1caf737d68daea152006fa2946859
Mark <mark.summerville@gmail.com>
2020-06-15 12:06:38 +0800

    cf

---
+++ TVRename/ScanActivity/Finders/DownloadingProviderFinder.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+namespace TVRename
+{
+    abstract class DownloadingProviderFinder : DownloadingFinder
+    {
+        private readonly IDownloadProvider source;
+        protected DownloadingProviderFinder(TVDoc doc, IDownloadProvider source) : base(doc)
+        {
+            this.source = source;
+        }
+        protected override string CheckName() => $"Looked in {source.Name()} for the missing files to see if they are being downloaded";
+
+        protected override void DoCheck(SetProgressDelegate prog, ICollection<ShowItem> showList, TVDoc.ScanSettings settings)
+        {
+            List<TorrentEntry>? downloading = source.GetTorrentDownloads();
+            if (downloading is null)
+            {
+                LOGGER.Warn($"Failed to get current downloads from {source.Name()}");
+                return;
+            }
+            SearchForAppropriateDownloads(downloading, DownloadApp.qBitTorrent, settings);
+        }
+    }
+}
