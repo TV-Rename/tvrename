793e3125b5642c4682f4f04ec338a551666d9051
Mark <mark.summerville@gmail.com>
2021-06-25 10:52:25 +0800

    Fix accordian issue where it does not close properly

--- TVRename/Utility/Helper/ShowHtmlHelper.cs
+++ TVRename/Utility/Helper/ShowHtmlHelper.cs
@@ -1473,7 +1473,7 @@ namespace TVRename
 
             if (f < .25)
             {
-                return "";
+                return string.Empty;
             }
 
             if (f <= .75)
@@ -1498,9 +1498,9 @@ namespace TVRename
                 <meta charset=""utf-8"">
                 <meta name = ""viewport"" content = ""width=device-width, initial-scale=1.0"" >
                 <title> TV Rename - Show Summary</title>
-                <link rel = ""stylesheet"" href = ""https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"" />
-                <link rel = ""stylesheet"" href = ""https://use.fontawesome.com/releases/v5.14.0/css/all.css"" />
-                </head >"
+                 <link href=""https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"" rel=""stylesheet"" integrity=""sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"" crossorigin=""anonymous"">
+                <link rel=""stylesheet"" href=""https://pro.fontawesome.com/releases/v5.10.0/css/all.css"" integrity=""sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"" crossorigin=""anonymous""/>
+                </head>"
                 + $"<body style=\"background-color: {backgroundColour.HexColour()}\" ><div class=\"col-sm-{size} offset-sm-{(12 - size) / 2}\">";
         }
 
@@ -1516,12 +1516,10 @@ namespace TVRename
         {
             return @"
                 </div>
-                <script src=""https://code.jquery.com/jquery-3.6.0.min.js"" integrity=""sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="" crossorigin=""anonymous""></script>
-                <script src=""https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js""></script>
-                <script src=""https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js""></script>
-				<script src=""https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js""></script>
-                <script>$(document).ready(function(){ })</script>
-                     </ body>
+                 <script src=""https://code.jquery.com/jquery-3.6.0.min.js"" integrity=""sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="" crossorigin=""anonymous""></script>
+				<script src=""https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"" integrity=""sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"" crossorigin=""anonymous""></script>
+                    <script>$(document).ready(function(){ })</script>
+                     </body>
                 </html>
                 ";
         }
