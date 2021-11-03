cd134573f808f67399d2f4b5eb00546e850f0c12
Mark <mark.summerville@gmail.com>
2021-11-03 22:45:19 +0800

    TMDB Error Handling

--- TVRename/Sources/TMDB/API.cs
+++ TVRename/Sources/TMDB/API.cs
@@ -6,6 +6,8 @@ using JetBrains.Annotations;
 using TMDbLib.Client;
 using TMDbLib.Objects.Changes;
 using TMDbLib.Objects.General;
+using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace TVRename.TMDB
 {
@@ -20,78 +22,69 @@ namespace TVRename.TMDB
         [NotNull]
         public static IEnumerable<ChangesListItem> GetChangesMovies(this TMDbClient client, CancellationToken cts, [NotNull] UpdateTimeTracker latestUpdateTime)
         {
-            //We need to ask for updates in blocks of 7 days
-            //We'll keep asking until we get to a date within 7 days of today
-            //(up to a maximum of 52 - if you are this far behind then you may need multiple refreshes)
-
-            List<ChangesListItem> updatesResponses = new();
-            int numberOfCallsMade = 0;
-
-            for (DateTime time = latestUpdateTime.LastSuccessfulServerUpdateDateTime();
-                time <= DateTime.Now;
-                time = time.AddDays(14)
-                )
-            {
-                int maxPage = 1;
-                for (int currentPage = 0; currentPage < maxPage; currentPage++)
-                {
-                    if (cts.IsCancellationRequested)
-                    {
-                        throw new CancelledException();
-                    }
-                    SearchContainer<ChangesListItem>? response = client.GetMoviesChangesAsync(currentPage, time, cancellationToken: cts).Result;
-                    numberOfCallsMade++;
-                    maxPage = response.TotalPages;
-                    updatesResponses.AddRange(response.Results);
-                    if (numberOfCallsMade > MAX_NUMBER_OF_CALLS)
-                    {
-                        throw new TooManyCallsException();
-                    }
-                }
-            }
-            return updatesResponses;
+            return GetChanges(client, cts, latestUpdateTime, client.GetMoviesChangesAsync);
         }
 
         [NotNull]
         public static IEnumerable<ChangesListItem> GetChangesShows(this TMDbClient client, CancellationToken cts, [NotNull] UpdateTimeTracker latestUpdateTime)
         {
-            //We need to ask for updates in blocks of 7 days
-            //We'll keep asking until we get to a date within 7 days of today
-            //(up to a maximum of 52 - if you are this far behind then you may need multiple refreshes)
-
-            List<ChangesListItem> updatesResponses = new();
-            int numberOfCallsMade = 0;
-
-            for (DateTime time = latestUpdateTime.LastSuccessfulServerUpdateDateTime();
-                time <= DateTime.Now;
-                time = time.AddDays(14)
-            )
-            {
-                int maxPage = 1;
-                for (int currentPage = 0; currentPage < maxPage; currentPage++)
-                {
-                    if (cts.IsCancellationRequested)
-                    {
-                        throw new CancelledException();
-                    }
-                    SearchContainer<ChangesListItem>? response = client.GetTvChangesAsync(currentPage, time, cancellationToken: cts).Result;
-                    numberOfCallsMade++;
-                    maxPage = response.TotalPages;
-                    updatesResponses.AddRange(response.Results);
-                    if (numberOfCallsMade > MAX_NUMBER_OF_CALLS)
-                    {
-                        throw new TooManyCallsException();
-                    }
-                }
-            }
-            return updatesResponses;
+            return GetChanges(client,cts,latestUpdateTime, client.GetTvChangesAsync);
         }
 
-        private class TooManyCallsException : Exception
+        private static IEnumerable<ChangesListItem> GetChanges(this TMDbClient client, CancellationToken cts, UpdateTimeTracker latestUpdateTime, Func<int, DateTime?, DateTime?, CancellationToken, Task<SearchContainer<ChangesListItem>>> changeMethod)
+        {
+            //We need to ask for updates in blocks of 14 days
+            //We'll keep asking until we get to a date within 14 days of today
+            //(up to a maximum of 52 - if you are this far behind then you may need multiple refreshes)
+            try
+            {
+                List<ChangesListItem> updatesResponses = new();
+                int numberOfCallsMade = 0;
+
+                for (DateTime time = latestUpdateTime.LastSuccessfulServerUpdateDateTime();
+                    time <= DateTime.Now;
+                    time = time.AddDays(14)
+                )
+                {
+                    int maxPage = 1;
+                    for (int currentPage = 0; currentPage < maxPage; currentPage++)
+                    {
+                        if (cts.IsCancellationRequested)
+                        {
+                            throw new CancelledException();
+                        }
+                        SearchContainer<ChangesListItem>? response = changeMethod(currentPage, time, null, cts).Result;
+                        numberOfCallsMade++;
+                        maxPage = response.TotalPages;
+                        updatesResponses.AddRange(response.Results);
+                        if (numberOfCallsMade > MAX_NUMBER_OF_CALLS)
+                        {
+                            throw new TooManyCallsException();
+                        }
+                    }
+                }
+                return updatesResponses;
+            }
+            catch (AggregateException aex) when (aex.InnerException is HttpRequestException ex)
+            {
+                throw new SourceConnectivityException(ex.Message);
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new SourceConnectivityException(ex.Message);
+            }
+            catch (Exception e)
+            {
+                throw new SourceConnectivityException(e.Message);
+            }
+        }
+
+
+        public class TooManyCallsException : Exception
         {
         }
 
-        private class CancelledException : Exception
+        public class CancelledException : Exception
         {
         }
 
