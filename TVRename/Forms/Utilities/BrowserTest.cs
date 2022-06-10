409f63c9c194233bc33756d22591463cef2cb463
Mark <mark.summerville@gmail.com>
2022-06-11 00:38:52 +0800

    Try to fix the webtest component

--- TVRename/Forms/Utilities/BrowserTest.cs
+++ TVRename/Forms/Utilities/BrowserTest.cs
@@ -9,23 +9,24 @@ namespace TVRename.Forms.Utilities
     public partial class BrowserTest : Form
     {
         [NotNull] private readonly string content;
+        private readonly CoreWebView2Environment webView2Environment;
 
         public BrowserTest([NotNull] string content)
         {
             this.content = content;
             InitializeComponent();
 
-            CoreWebView2Environment webView2Environment = 
-                 CoreWebView2Environment.CreateAsync(null,
-                    PathManager.CefCachePath).Result;
-            webEdge.EnsureCoreWebView2Async(webView2Environment).RunSynchronously();
+            webView2Environment = CoreWebView2Environment.CreateAsync(
+                     null,
+                            PathManager.CefCachePath
+                     ).Result;
         }
 
         private async void BrowserTest_Load(object sender, EventArgs e)
         {
             webChrome.Load("data:text/html;base64," + Convert.ToBase64String(Encoding.UTF8.GetBytes(content)));
 
-            await webEdge.EnsureCoreWebView2Async();
+            await webEdge.EnsureCoreWebView2Async(webView2Environment);
             webEdge.NavigateToString(content);
         }
 
