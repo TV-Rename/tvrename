f82edc564a28338e9d622b5aece24efad1d62004
Mark <mark.summerville@gmail.com>
2022-07-10 18:25:06 +0800

    Fix issue that means that failure to open webpage occasionally causes a crash

--- TVRename/Utility/Helper/Helpers.cs
+++ TVRename/Utility/Helper/Helpers.cs
@@ -232,30 +232,39 @@ public static class Helpers
     {
         try
         {
-            Process.Start(url);
+            try
+            {
+                Process.Start(url);
+            }
+            catch (Exception e)
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    //url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    Logger.Error(e, $"Could not open URL: {url}");
+                    return false;
+                }
+            }
         }
         catch (Exception e)
         {
-            // hack because of this: https://github.com/dotnet/corefx/issues/10361
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                //url = url.Replace("&", "^&");
-                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                Process.Start("xdg-open", url);
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                Process.Start("open", url);
-            }
-            else
-            {
-                Logger.Error(e, $"Could not open URL: {url}");
-                return false;
-            }
+            Logger.Error(e, $"Could not open URL: {url}");
+            return false;
         }
+
         return true;
     }
 
