a469d0de35d96ae8278325aec5e8f25ddda24c0a
Mark <mark.summerville@gmail.com>
2022-09-26 00:29:53 +0800

    tweak

--- TVRename/ItemsAndActions/ActionPyTivoMeta.cs
+++ TVRename/ItemsAndActions/ActionPyTivoMeta.cs
@@ -6,7 +6,6 @@
 // Copyright (c) TV Rename. This code is released under GPLv3 https://github.com/TV-Rename/tvrename/blob/master/LICENSE.md
 //
 
-using System.Linq;
 using System.Threading;
 
 namespace TVRename;
